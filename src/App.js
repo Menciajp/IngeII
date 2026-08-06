70ef887712c08853ca5bca03804d5904a8611cdd
Santiago Valentino Aegerter <valentinoaegerter12@gmail.com>
2026-08-06 15:11:25 -0300

    chore: actualiza importaciones finales en App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import ReportePagos from './screens/ReportePagos/ReportePagos';
 import GestionAlumnos from './screens/GestionAlumnos/GestionAlumnos';
 import AsignarRutina from './screens/AsignarRutina/AsignarRutina';
 import AgregarRutina from './screens/AgregarRutina/AgregarRutina';
+import ConsultaAlumno from './screens/ConsultaAlumno/ConsultaAlumno';
+import MiRutina from './screens/MiRutina/MiRutina';
 
 function App() {
   return (
     <div className="App">
-      <AgregarRutina />
+      {/* Comentá o descomentá la pantalla que quieras ir probando */}
+      {/* <ReportePagos /> */}
+      {/* <GestionAlumnos /> */}
+      {/* <AsignarRutina /> */}
+      {/* <AgregarRutina /> */}
+      {/* <ConsultaAlumno /> */}
+      <MiRutina />
     </div>
   );
 }
